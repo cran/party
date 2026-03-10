111566d9f41cb24ef3059ec4b9e6ca470d84a1dd
Torsten Hothorn <Torsten.Hothorn@R-project.org>
2026-03-10 12:50:02 +0000

    version 1.3-19

--- inst/doc/MOB.R
+++ inst/doc/MOB.R
@@ -8,53 +8,53 @@ options(useFancyQuotes = FALSE)
 
 
 ###################################################
-### code chunk number 2: MOB.Rnw:232-233
+### code chunk number 2: MOB.Rnw:229-230
 ###################################################
 library("party")
 
 
 ###################################################
-### code chunk number 3: MOB.Rnw:238-239
+### code chunk number 3: MOB.Rnw:235-236
 ###################################################
 data("BostonHousing", package = "mlbench")
 
 
 ###################################################
-### code chunk number 4: MOB.Rnw:253-255
+### code chunk number 4: MOB.Rnw:250-252
 ###################################################
 BostonHousing$lstat <- log(BostonHousing$lstat)
 BostonHousing$rm <- BostonHousing$rm^2
 
 
 ###################################################
-### code chunk number 5: MOB.Rnw:270-272
+### code chunk number 5: MOB.Rnw:267-269
 ###################################################
 BostonHousing$chas <- factor(BostonHousing$chas, levels = 0:1, labels = c("no", "yes"))
 BostonHousing$rad <- factor(BostonHousing$rad, ordered = TRUE)
 
 
 ###################################################
-### code chunk number 6: MOB.Rnw:283-285
+### code chunk number 6: MOB.Rnw:280-282
 ###################################################
 ctrl <- mob_control(alpha = 0.05, bonferroni = TRUE, minsplit = 40,
   objfun = deviance, verbose = TRUE)
 
 
 ###################################################
-### code chunk number 7: MOB.Rnw:298-300
+### code chunk number 7: MOB.Rnw:295-297
 ###################################################
 fmBH <- mob(medv ~ lstat + rm | zn + indus + chas + nox + age + dis + rad + tax + crim + b + ptratio,
   data = BostonHousing, control = ctrl, model = linearModel)
 
 
 ###################################################
-### code chunk number 8: MOB.Rnw:309-310
+### code chunk number 8: MOB.Rnw:306-307
 ###################################################
 fmBH
 
 
 ###################################################
-### code chunk number 9: MOB.Rnw:316-317 (eval = FALSE)
+### code chunk number 9: MOB.Rnw:313-314 (eval = FALSE)
 ###################################################
 ## plot(fmBH)
 
@@ -66,25 +66,25 @@ plot(fmBH)
 
 
 ###################################################
-### code chunk number 11: MOB.Rnw:350-351
+### code chunk number 11: MOB.Rnw:347-348
 ###################################################
 coef(fmBH)
 
 
 ###################################################
-### code chunk number 12: MOB.Rnw:357-358
+### code chunk number 12: MOB.Rnw:354-355
 ###################################################
 summary(fmBH, node = 7)
 
 
 ###################################################
-### code chunk number 13: MOB.Rnw:365-366
+### code chunk number 13: MOB.Rnw:362-363
 ###################################################
 sctest(fmBH, node = 7)
 
 
 ###################################################
-### code chunk number 14: MOB.Rnw:372-375
+### code chunk number 14: MOB.Rnw:369-372
 ###################################################
 mean(residuals(fmBH)^2)
 logLik(fmBH)
@@ -92,28 +92,28 @@ AIC(fmBH)
 
 
 ###################################################
-### code chunk number 15: MOB.Rnw:378-380
+### code chunk number 15: MOB.Rnw:375-377
 ###################################################
 nt <- NROW(coef(fmBH))
 nk <- NCOL(coef(fmBH))
 
 
 ###################################################
-### code chunk number 16: MOB.Rnw:396-398
+### code chunk number 16: MOB.Rnw:393-395
 ###################################################
 data("PimaIndiansDiabetes2", package = "mlbench")
 PimaIndiansDiabetes <- na.omit(PimaIndiansDiabetes2[,-c(4, 5)])
 
 
 ###################################################
-### code chunk number 17: MOB.Rnw:418-420
+### code chunk number 17: MOB.Rnw:415-417
 ###################################################
 fmPID <- mob(diabetes ~ glucose | pregnant + pressure + mass + pedigree + age,
   data = PimaIndiansDiabetes, model = glinearModel, family = binomial())
 
 
 ###################################################
-### code chunk number 18: MOB.Rnw:425-426 (eval = FALSE)
+### code chunk number 18: MOB.Rnw:422-423 (eval = FALSE)
 ###################################################
 ## plot(fmPID)
 
@@ -125,14 +125,14 @@ plot(fmPID)
 
 
 ###################################################
-### code chunk number 20: MOB.Rnw:460-462
+### code chunk number 20: MOB.Rnw:457-459
 ###################################################
 coef(fmPID)
 exp(coef(fmPID)[,2])
 
 
 ###################################################
-### code chunk number 21: MOB.Rnw:465-466
+### code chunk number 21: MOB.Rnw:462-463
 ###################################################
 risk <- round(100 * (exp(coef(fmPID)[,2])-1), digits = 1)
 
